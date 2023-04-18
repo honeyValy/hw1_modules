77cbb2a4015bd9317e78d2edfe1fbc5a412f5277
Valeria Chernova <via.chernova@mail.ru>
2023-04-18 23:50:35 +0300

    initial commit

---
+++ application/db/people.py
@@ -0,0 +1,6 @@
+def get_employees():
+    print('called function get_employees')
+
+
+if __name__ == 'main':
+    get_employees()
---
+++ application/salary.py
@@ -0,0 +1,6 @@
+def calculate_salary():
+    print('called function calculate_salary')
+
+
+if __name__ == 'main':
+    calculate_salary()
